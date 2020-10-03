a9a9491dfdb6b480345154e3a7e9cd4f41bc9d60
Lukas Langrock <lukas.langrock@outlook.de>
2020-10-03 18:40:35 +0200

    Remove github icon

--- Shutdown Timer/Properties/Resources.Designer.cs
+++ Shutdown Timer/Properties/Resources.Designer.cs
@@ -71,16 +71,6 @@ namespace ShutdownTimer.Properties {
         }
         
         /// <summary>
-        ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap github {
-            get {
-                object obj = ResourceManager.GetObject("github", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
         ///   Sucht eine lokalisierte Ressource vom Typ System.Drawing.Icon ähnlich wie (Symbol).
         /// </summary>
         internal static System.Drawing.Icon icon_dark {
