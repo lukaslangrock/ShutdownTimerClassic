cfde99adc94c0542708ce71b6dce8f0cf09ac7cb
Lukas Langrock <lukas.langrock@outlook.de>
2025-02-03 14:45:24 +0100

    It's 2025

--- src/ShutdownTimer/Properties/AssemblyInfo.cs
+++ src/ShutdownTimer/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Lukas Langrock")]
 [assembly: AssemblyProduct("ShutdownTimerClassic")]
-[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyCopyright("Copyright © 2025")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
